f4436179c6b6d34d167edd65462769746ebe0b5f
amnakhan82@gmail.com <38444180+1bluebird@users.noreply.github.com>
2020-12-02 16:48:44 -0800

    source code for GCP

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const fetch = require("node-fetch");
+//setting the environemet variables via code for Google speech API
+const projectId = "transcriber-295201";
+const keyFile = "public/transcriber-295201-97ece22b522e.json";
+//including the Speech API
+const speech = require('@google-cloud/speech');
+const client = new speech.SpeechClient({projectId, keyFile});
+// Imports the Google Cloud client library
+const {Translate} = require('@google-cloud/translate').v2;
+// Creates a client
+const translate = new Translate({keyFilename: keyFile});
+
+
+
+port = 3400;
+host = '0.0.0.0';
+//host = '127.0.0.20';
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use("/scripts", express.static(__dirname + '/public/javascripts'));
+app.use(express.json({ limit:'5000', type:'application/json'}));
+
+
+app.get('/', function (req, res) {
+      res.sendFile(__dirname + '/public/filePicker.html');
+});
+
+
+app.get('/transcription/:id', function (req, res) {
+      let fileid = req.params.id;
+      console.log(`the file id recieved is ${fileid}`);
+      let url = `https://drive.google.com/uc?export=download&id=${fileid}`;
+      fetch(url)
+                .then(response => response.buffer())
+                .then(buf => buf.toString('base64'))
+                .then(async (data) =>  {
+                      const audio = {
+                          content: data,
+                        };
+
+                        const config = {
+                          encoding: 'LINEAR16',
+                          sampleRateHertz: 8000,
+                          languageCode: 'en-US'
+                        };
+
+                        const request = {
+                          audio: audio,
+                          config: config
+                        };
+
+                        const [response] = await client.recognize(request);
+                        const transcription = response.results
+                          .map(result => result.alternatives[0].transcript)
+                          .join('\n');
+                        res.send(transcription);})
+                  .catch(function(error) {
+                        console.log(error);
+                          });
+
+});
+
+
+app.post('/translate/:tlang', function (req, res){
+         console.log(`A POST request recieved: `);
+         let target = req.params.tlang;
+         let text = req.body.text;
+
+           translate.translate(text, target).then(function(translation){
+           console.log(translation[0]);
+          res.send(translation[0]);
+        });
+  });
+
+
+
+
+app.listen(port, host, function(){
+  console.log(`Server listening on IPv4: ${host}: ${port}`)
+});
---
+++ public/javascripts/drivePicker.js
@@ -0,0 +1,160 @@
+
+// The API key obtained from the Google API Console.
+// Replace with your own API key, or your own key.
+var API_KEY = "AIzaSyD96mtmpJwDOC20WNOXgrXjL59Xt4gwxwU";
+
+// The Client ID obtained from the Google API Console. Replace with your own Client ID.
+var CLIENT_ID = "587413294405-5jrj5qa18ni42cs5bmrrvutve9e6vdl3.apps.googleusercontent.com";
+
+// The APP ID obtained from the Google API Console. Replace with your own APP ID and
+// keep in sync with API_KEY and CLIENT_ID.
+var APP_ID = "transcriber-295201";
+
+// API discovery doc URL for APIs used by this example
+var DISCOVERY_DOCS = "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest";
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/drive.file';
+
+var GoogleAuth;
+var GoogleUser;
+var pickFileButton = document.getElementById('pick_file_button');
+var transcribeButton = document.getElementById('transcribe_button');
+var translateButton = document.getElementById('translate_button');
+var fileText = document.getElementById('content');
+var transcribeText = document.getElementById('transcribed-content');
+var selectedLang = document.getElementById('lang_select');
+var translateText = document.getElementById('translated-content');
+
+
+pickFileButton.onclick = handlePickFileButtonClick;
+transcribeButton.onclick = handleTranscribeVideoClick;
+translateButton.onclick = handleTranslate;
+
+/**
+ *  On load, called to load the API client library, the auth2 library, and the picker library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2:picker', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets the authorization and current user.
+ */
+function initClient() {
+  gapi.client.init({
+    client_id: CLIENT_ID,
+    discoveryDocs: [DISCOVERY_DOCS],
+    scope: SCOPES
+  }).then(function () {
+    GoogleAuth = gapi.auth2.getAuthInstance();
+    GoogleUser = GoogleAuth.currentUser.get();
+    pickFileButton.style.display = 'block';
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2), transcribeText);
+  });
+}
+
+/**
+ * Ensures the user has the correct scopes prior to displaying the picker.
+ */
+function handlePickFileButtonClick() {
+  if (!GoogleUser.hasGrantedScopes(SCOPES)) {
+    GoogleUser.grant({
+      scope: SCOPES
+    }).then(displayPicker, function(err) {
+      // Handle error/declined auth...
+    });
+  } else {
+    displayPicker();
+  }
+}
+
+/**
+ * Create and render a Picker for selecting any file on My Drive.
+ * We use .enableFeature(google.picker.Feature.SUPPORT_DRIVES)
+ * to ensure files in shared drives are included.
+ */
+function displayPicker() {
+  // Fetch current token
+  let authResponse = GoogleUser.getAuthResponse(true);
+
+  var view = new google.picker.View(google.picker.ViewId.DOCS);
+  var picker = new google.picker.PickerBuilder()
+    .enableFeature(google.picker.Feature.SUPPORT_DRIVES)
+    .setAppId(APP_ID)
+    .setOAuthToken(authResponse.access_token)
+    .addView(view)
+    .setDeveloperKey(API_KEY)
+    .setCallback(pickerCallback)
+    .build();
+  picker.setVisible(true);
+}
+
+/**
+ * Called when a file is picked. This function gathers metadata from
+ * the picked documents and from calling get on the fileId.
+ */
+function pickerCallback(data) {
+  var document = data[google.picker.Response.DOCUMENTS][0];
+  // The following information is passed to the callback, while other has to be
+  // retrieved by calling files.get() on a file (below)
+  var fileName = document[google.picker.Document.NAME];
+  var URL = document[google.picker.Document.URL];
+  var lastModifiedDate = document[google.picker.Document.LAST_EDITED_UTC];
+  var date = new Date(lastModifiedDate);
+  window.fileId = document[google.picker.Document.ID];
+
+  appendPre('Audio File: ' + fileName, fileText);
+  //appendPre('Last Modified Date: ' + date);
+  transcribe_button.style.display = 'block';
+  transcribeText.style.display ='block';
+}
+/**
+ * Helper method to display content to the screen.
+ */
+
+
+function appendPre(message, fieldPre) {
+      if (fieldPre.hasChildNodes()) {
+       fieldPre.removeChild(fieldPre.childNodes[0]);
+    }
+      var textContent = document.createTextNode(message + '\n');
+      fieldPre.appendChild(textContent);
+}
+
+
+
+function handleTranscribeVideoClick(){
+      let id =  window.fileId;
+      fetch(`/transcription/${id}`)
+          .then(response => response.text())
+          .then((data) => {
+                appendPre(data, transcribeText);
+                window.textEnglish = data;
+                      })
+          .catch(e => {
+            console.log("The error is: ", e);
+            appendPre(`Error: Please select the correct format for an audio file!`, transcribeText);
+          });
+}
+
+
+
+function handleTranslate(){
+    //var text = window.textEnglish;
+        var langTarget = selectedLang.value;
+
+        fetch(`/translate/${langTarget}`, {
+                  method: 'POST',
+                  headers: {"Content-type": "application/json"},
+                  body: JSON.stringify({
+                        text: window.textEnglish })
+                  }).then(response => response.text())
+                  .then((data) => {
+                        appendPre(data, translateText);
+                  }).catch(e => {
+                       console.log("The error is: ", e);
+                     });
+}
